a607ab46e69d9cdd6dcee6037c97a512add2fe9c
Selvaganesh K <Selvaganesh.eie@gmail.com>
2017-01-25 18:08:27 +0530

    Add files via upload

---
+++ client.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Layout from "./components/Layout";
+
+
+class Layout extends React.Component {
+    render(){
+        return{
+            <h1>Hai!</h1>
+        };
+    }
+}
+
+
+const app = document.getElementById('app');
+ReactDOM.render(<Layout/>, app);
